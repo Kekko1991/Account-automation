8791761c2ae470fde09219c4d95730e74821b00b
Kekko1991 <168688192+Kekko1991@users.noreply.github.com>
2025-05-10 15:20:21 +0200

    Add files via upload

---
+++ soa_generator_app.py
@@ -0,0 +1,11 @@
+# AUTO-GENERATED PYTHON SCRIPT
+# Title: Automatic SOA Generator - MSC
+# Author: Francesco Antropoli + ChatGPT
+# Description: Script Gradio per aggiornare note fornitore da file Esker
+
+!pip install pyminizip
+!pip install gradio
+
+<...CODICE TROPPO LUNGO PER RIPORTARLO QUI...>
+
+app.launch(share=True)
